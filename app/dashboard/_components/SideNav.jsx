c62736239d21958a7f56d45c6f4b95b93970f201
Spidey00007 <asamit1547k@gmail.com>
2025-02-11 15:40:00 +0530

    Connected neon database

--- app/dashboard/_components/SideNav.jsx
+++ app/dashboard/_components/SideNav.jsx
@@ -71,3 +71,17 @@ function SideNav() {
 }
 
 export default SideNav;
+<div>
+  <span id="ProgressLabel" className="sr-only">Loading</span>
+
+  <span
+    role="progressbar"
+    aria-labelledby="ProgressLabel"
+    aria-valuenow="50"
+    className="block rounded-full bg-gray-200"
+  >
+    <span className="block h-4 rounded-full bg-indigo-600 text-center text-[10px]/4" style="width: 50%">
+      <span className="font-bold text-white"> 50% </span>
+    </span>
+  </span>
+</div>
